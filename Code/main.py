a9c3fe4b4d457c03de6ed3bb9330d931c8f46b59
Lucas <l.kneffel-otal@tu-braunschweig.de>
2021-03-21 10:02:41 +0100

    loadDataFunction

--- Code/main.py
+++ Code/main.py
@@ -1,9 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sklearn as sk
+import pandas as pd
+
+
+def loadData():
+
+    datasets = {
+        "a": [],
+        "b": [],
+        "c": []
+    }
+
+    for i in ["a", "b", "c"]:
+        datasets[i] = pd.read_excel("../Dataset/trainingdata_{}.xls".format(i))
+        print("Dataset {}".format(i))
+        print(datasets[i])
+
+    return datasets
 
 def main():
     print('Start Coding, Have fun!')
-    pass
+    datasets = loadData()
+
+
+
+
+
 
 
 
