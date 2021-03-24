d772819d0fce23b5b30f80da7388787d3f27f759
Lucas <l.kneffel-otal@tu-braunschweig.de>
2021-03-24 08:48:57 +0100

    insert_Dataplot

--- Code/main.py
+++ Code/main.py
@@ -14,14 +14,60 @@ def loadData():
 
     for i in ["a", "b", "c"]:
         datasets[i] = pd.read_excel("../Dataset/trainingdata_{}.xls".format(i))
-        print("Dataset {}".format(i))
-        print(datasets[i])
 
     return datasets
+
+
+def DataPlot(data):
+    X = []
+    Y = []
+    Lables = []
+    data_num = 0
+    for i in ["a", "b", "c"]:
+        Classes = {
+            "red": [],
+            "blue": []
+        }
+
+        dataset = data[i]
+        X.append(dataset["x_i1"])
+        Y.append(dataset["x_i2"])
+        Lables.append(dataset["l_i"])
+
+        index = 0
+        for label in Lables[data_num]:
+            if label == 1:
+                Classes["red"].append([X[data_num][index], Y[data_num][index]])
+            else:
+                Classes["blue"].append([X[data_num][index], Y[data_num][index]])
+            index = index + 1
+
+        fig = plt.figure()
+        
+        x_red = []
+        y_red = []
+        for x, y in Classes["red"]:
+            x_red.append(x)
+            y_red.append(y)
+        plt.plot(x_red,y_red, 'or')
+
+        x_blue = []
+        y_blue = []
+        for x, y in Classes["blue"]:
+            x_blue.append(x)
+            y_blue.append(y)
+        plt.plot(x_blue, y_blue, 'ob')
+
+        fig.savefig("../Dataset/data_{}".format(i), dpi=300)
+        data_num = data_num + 1
+
+
 
 def main():
     print('Start Coding, Have fun!')
     datasets = loadData()
+    DataPlot(datasets)
+
 
 
 
