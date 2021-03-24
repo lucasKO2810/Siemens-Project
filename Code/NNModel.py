663626ceecdfb458225cc985d7ccd5d637916327
Lucas <l.kneffel-otal@tu-braunschweig.de>
2021-03-24 09:23:51 +0100

    NNCLass

---
+++ Code/NNModel.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+
+
+
+    def forward(self):
+        pass
+    pass
