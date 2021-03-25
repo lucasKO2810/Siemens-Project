dcff6186a3664994808ebbc30b593de2d8f628b9
Lucas <l.kneffel-otal@tu-braunschweig.de>
2021-03-25 23:51:22 +0100

    Fehler_in_Line_135_main.py

--- Code/NNModel.py
+++ Code/NNModel.py
@@ -10,7 +10,6 @@ class Net(nn.Module):
 
         self.fl1 = nn.Linear(2, 64)
         self.fl2 = nn.Linear(64, 128)
-        self.dropout1 = nn.Dropout2d(0.1)
         self.fl3 = nn.Linear(128, 64)
         self.output = nn.Linear(64, 1)
 
@@ -18,15 +17,13 @@ class Net(nn.Module):
 
     def forward(self, x):
 
-        x = torch.from_numpy(x)
-
         x = self.fl1(x)
         x = F.relu_(x)
         x = self.fl2(x)
         x = F.relu_(x)
-        x = self.dropout1(x)
         x = self.fl3(x)
         x = F.relu_(x)
         x = self.output(x)
         x = F.softmax(x)
-    pass
+
+        return x
--- Code/main.py
+++ Code/main.py
@@ -7,6 +7,27 @@ from sklearn.metrics import classification_report, confusion_matrix
 import NNModel
 import torch
 
+class Dataset(torch.utils.data.Dataset):
+  'Characterizes a dataset for PyTorch'
+  def __init__(self, list_IDs, labels):
+        'Initialization'
+        self.labels = labels
+        self.list_IDs = list_IDs
+
+  def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.list_IDs)
+
+  def __getitem__(self, index):
+        'Generates one sample of data'
+        # Select sample
+        ID = self.list_IDs[index]
+
+        # Load data and get label
+        X = torch.load('data/' + ID + '.pt')
+        y = self.labels[ID]
+
+        return X, y
 
 def loadData():
 
@@ -88,14 +109,43 @@ def main():
         print(classification_report(label_test, label_pred))
 
         ######### Neural Network
-
-        device = torch.device("cpu")
+        #input = torch.tensor(input.values)
+        #label = torch.tensor(label)
+        trainset = Dataset(input, label)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                    shuffle=True, num_workers=2)
 
         model = NNModel.Net()
         loss_fn = torch.nn.MSELoss(reduction='sum')
-
         learning_rate = 1e-3
         optimizer = torch.optim.RMSprop(model.parameters(), lr=learning_rate)
+
+
+        for epoch in range(0,10):
+            running_loss = 0.0
+            index = 0
+            for k in range(0, len(trainloader.dataset)):
+            #for k, data in enumerate(trainloader, 0):
+                #inputs, labels = data
+                inputs = torch.tensor(trainloader.dataset.list_IDs.iloc[k].astype('float32'))
+                labels = torch.tensor(trainloader.dataset.labels.iloc[k])
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = loss_fn(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                if index % 2000 == 1999:  # print every 2000 mini-batches
+                    print('[%d, %5d] loss: %.3f' %
+                          (epoch + 1, index + 1, running_loss / 2000))
+                    running_loss = 0.0
+
+            index = index + 1
+
+        print('Finished Training')
+
+
+
 
 
 
