67d865e26dc5737b6136812e8d37245a83a3f60b
allaaelkhodr <59017414+allaaelkhodr@users.noreply.github.com>
2021-03-27 13:33:44 +0100

    add parser arguments

--- Code/main.py
+++ Code/main.py
@@ -14,6 +14,9 @@ import torch.nn.functional as F
 from utils.Dataset import NewDataset
 from Model.NNModel import Net
 
+import pickle
+from argparse import ArgumentParser
+
 
 def loadData():
 
@@ -72,7 +75,8 @@ def dataPlot(data):
         fig.savefig("../Dataset/data_{}".format(i), dpi=300)
         data_num = data_num + 1
 
-def train(train_data, model, i):
+
+def train_nn(train_data, model, i):
     optimizer = optim.RMSprop(model.parameters(), lr=0.01)
     loss_fn = F.mse_loss
 
@@ -94,7 +98,8 @@ def train(train_data, model, i):
     print("Training {} Finished".format(i))
     print(" ")
 
-def test(test_data, model, i):
+
+def test_nn(test_data, model, i):
     correct = 0
     total = 0
     with torch.no_grad():
@@ -108,6 +113,7 @@ def test(test_data, model, i):
     print('Accuracy of the network on the 10000 test images: %d %%' % (
             100 * correct / total))
 
+
 def confidence(test_data, model, svm, i):
     distance = []
     for batch, (data, target) in enumerate(test_data):
@@ -120,29 +126,57 @@ def confidence(test_data, model, svm, i):
 
     return distance
 
-def main():
+
+def train_svm(input_train, label_train, i):
+    # ----------------------------------------------------------------------
+    # Train SVM
+    # ----------------------------------------------------------------------
+    svclassifier = SVC(kernel='rbf')
+    svclassifier.fit(input_train, label_train)
+    # ----------------------------------------------------------------------
+    # Save SVM
+    # ----------------------------------------------------------------------
+    filename = './Model/svm_model_{}.sav'.format(i)
+    pickle.dump(svclassifier, open(filename, 'wb'))
+
+    return svclassifier
+
+
+def main(args):
     print('Start Coding, Have fun!')
+    # ----------------------------------------------------------------------
+    # Save parameters
+    # ----------------------------------------------------------------------
+    loadSVM = args.loadSVM
+    loadNN = args.loadNN
+
     datasets = loadData()
     test_split = 0.1
-    #dataPlot(datasets)
     for i in ["a", "b", "c"]:
-
+        # ----------------------------------------------------------------------
+        # Get inputs and targets & split data
+        # ----------------------------------------------------------------------
         input = datasets[i][["x_i1", "x_i2"]]
         label = datasets[i]['l_i']
-
-        ###### SVM Training
         input_train, input_test, label_train, label_test = train_test_split(input, label, test_size=test_split)
-        svclassifier = SVC(kernel='rbf')
-        svclassifier.fit(input_train, label_train)
-
+        # ----------------------------------------------------------------------
+        # Load or train SVM model
+        # ----------------------------------------------------------------------
+        if loadSVM:
+            svclassifier = pickle.load(open('./Model/svm_model_{}.sav'.format(i), 'rb'))
+            print("----- SVM model {} succesfully loaded -----".format(i))
+        else:
+            svclassifier = train_svm(input_train, label_train, i)
+        # ----------------------------------------------------------------------
+        # Test SVM model
+        # ----------------------------------------------------------------------
         label_pred = svclassifier.predict(input_test)
-
         print("SVM results  for Dataset {}".format(i))
         print(confusion_matrix(label_test, label_pred))
         print(classification_report(label_test, label_pred))
-
-        ######### Neural Network
-
+        # ----------------------------------------------------------------------
+        # Load and split data for NN
+        # ----------------------------------------------------------------------
         set = NewDataset('../Dataset/trainingdata_{}.xls'.format(i))
 
         train_range = list(range(0, int(len(set) - test_split * len(set))))
@@ -156,21 +190,26 @@ def main():
 
         test_data = torch.utils.data.DataLoader(tes_set, batch_size=1,
                                                 shuffle=True)
-
-        model = Net()
-        train(train_data, model, i)
-
-        ###### Test the model
-        PATH = './Model/model_{}_net.pth'.format(i)
-        trained_model = Net()
-        trained_model.load_state_dict(torch.load(PATH))
-        test(test_data, trained_model, i)
-
-
-        ####### Cofidence// Distrust
-        distrust = confidence(test_data, trained_model, svclassifier, i)
+        # ----------------------------------------------------------------------
+        # Load or train Neural Network
+        # ----------------------------------------------------------------------
+        if loadNN:
+            PATH = './Model/model_{}_net.pth'.format(i)
+            model = Net()
+            model.load_state_dict(torch.load(PATH))
+            print("----- Neural Network {} succesfully loaded -----".format(i))
+        else:
+            model = Net()
+            train_nn(train_data, model, i)
+        # ----------------------------------------------------------------------
+        # Test Neural Network
+        # ----------------------------------------------------------------------
+        test_nn(test_data, model, i)
+        # ----------------------------------------------------------------------
+        # Compute confidence/distrust
+        # ----------------------------------------------------------------------
+        distrust = confidence(test_data, model, svclassifier, i)
         print(distrust)
-
         fig_distrust = plt.figure()
         data_number = np.linspace(0, len(distrust), num=len(distrust))
         plt.xlabel("Sample")
@@ -179,5 +218,21 @@ def main():
         plt.show()
         fig_distrust.savefig("../Distrust_Data_{}".format(i), dpi=300)
 
+
+def options():
+    parser = ArgumentParser()
+    parser.add_argument('--loadSVM',
+                        type=int,
+                        default=1,  # True
+                        choices=[0, 1],
+                        help="Load SVM model")
+    parser.add_argument('--loadNN',
+                        type=int,
+                        default=1,  # True
+                        choices=[0, 1],
+                        help="Load Neural Network")
+    return parser.parse_args()
+
+
 if __name__ =="__main__":
-    main()
+    main(options())
